f7ee24d8756c3e1b9a3db06ef0d925382d6f1ad0
panbaoqiang <pbqhfut566@163.com>
2020-05-09 14:05:02 +0800

    update thermostatSetting

---
+++ src/views/thermostatSetting/component/thermostatUpdate.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <el-dialog title="温控编辑" :visible.sync="dialogUpdateThermostatVisible" :show-close="false" :close-on-click-modal="false">
+      <el-form  :model="thermostat" label-width="100px" class="demo-ruleForm">
+        <el-row :gutter="50">
+          <el-col :span="8" :offset="4"><el-form-item label="名称" prop="name">
+            <el-input v-model="thermostat.name" style="width:250px" />
+          </el-form-item>
+          </el-col>
+        </el-row>
+
+
+        <el-row :gutter="50">
+          <el-col :span="8" :offset="4">
+            <el-form-item label="设定PV" prop="path">
+              <el-input v-model="thermostat.pv" style="width:250px" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+
+        <el-row :gutter="50">
+          <el-col :span="8" :offset="4"><el-form-item label="设定SV" prop="remark">
+            <el-input v-model="thermostat.sv" style="width:250px" />
+          </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="50">
+          <el-col :span="8" :offset="4"><el-form-item label="上限" prop="remark">
+            <el-input v-model="thermostat.upLimmit" style="width:250px" />
+          </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="50">
+          <el-col :span="8" :offset="4"><el-form-item label="下限" prop="remark">
+            <el-input v-model="thermostat.lowLimmit" style="width:250px" />
+          </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="50">
+          <el-col :span="8" :offset="4"><el-form-item label="是否报警" prop="remark">
+            <el-input v-model="thermostat.isAlarm" style="width:250px" />
+          </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="50">
+          <el-col :span="8" :offset="4"><el-form-item label="报警灯号" prop="remark">
+            <el-input v-model="thermostat.Light" style="width:250px" />
+          </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="50">
+          <el-col :span="8" :offset="4"><el-form-item label="启用状态" prop="remark">
+            <el-input v-model="thermostat.status" style="width:250px" />
+          </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="50">
+          <el-col :span="4" :offset="4">
+            <el-form-item>
+              <el-button type="primary" @click="submitForm">保存</el-button>
+            </el-form-item>
+          </el-col>
+          <el-col :span="3">
+            <el-form-item>
+              <el-button @click="CloseForm()">关闭</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import store from '@/store'
+export default {
+  name: 'thermostatUpdate',
+  props: {
+    thermostat: {
+      type: Object,
+      default() {
+        return {
+          name:'',
+          pv:'',
+          sv:'',
+          upLimit:'',
+          lowLimmit:'',
+          isAlarm:'',
+          Light:'',
+          status:''
+        }
+      }
+    },
+    dialogUpdateThermostatVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+  methods: {
+    submitForm() {
+
+    },
+    CloseForm() {
+      // 关闭窗口
+      this.$emit('closeDialogUpdateThermostatVisible')
+    },
+      handleClipboard(text, event) {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.icons-container {
+  margin: 0;
+  overflow: hidden;
+  .grid {
+    position: relative;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(50px, 1fr));
+  }
+
+  .icon-item {
+    margin: 20px;
+    height: 60px;
+    text-align: center;
+    width: 20px;
+    float: left;
+    font-size: 25px;
+    color: #24292e;
+    cursor: pointer;
+  }
+
+  span {
+    display: block;
+    font-size: 16px;
+    margin-top: 10px;
+  }
+
+  .disabled {
+    pointer-events: none;
+  }
+}
+</style>
--- src/views/thermostatSetting/index.vue
+++ src/views/thermostatSetting/index.vue
@@ -1,14 +1,234 @@
 <template>
   <div >
-    温控设置页面
+           <el-row>
+          <el-col :span="24" >
+              <el-table
+              :data="tableData"
+              stripe
+              >
+              <el-table-column
+                prop="name"
+                label="名称"
+
+                 align="center"
+                >
+              </el-table-column>
+              <el-table-column
+                prop="pv"
+                label="实际PV值"
+
+                 align="center"
+                >
+              </el-table-column>
+              <el-table-column
+                prop="sv"
+                label="设定SV"
+
+                 align="center"
+                >
+              </el-table-column>
+              <el-table-column
+                prop="upLimmit"
+                label="上限"
+
+                 align="center"
+                >
+              </el-table-column>
+              <el-table-column
+                prop="lowLimmit"
+                label="下限"
+                 align="center"
+                >
+              </el-table-column>
+              <el-table-column
+                prop="isAlarm"
+                label="是否报警"
+                 align="center"
+                >
+              </el-table-column>
+              <el-table-column
+                  prop="Light"
+                  label="报警灯号"
+                  align="center"
+                >
+              </el-table-column>
+              <el-table-column
+                  prop="status"
+                  label="启用状态"
+                  align="center"
+                >
+              </el-table-column>
+              <el-table-column
+                label="操作"
+                 align="center"
+                >
+                <template slot-scope="scope">
+                    <el-link  type="warning" style="margin-left:20px" @click="updateThermostat(scope.row)"><i class="el-icon-edit" />编辑</el-link>
+                 </template>    
+              </el-table-column>
+            </el-table>
+          </el-col>
+      </el-row>
+      <thermostatUpdate :thermostat="thermostat"  :dialogUpdateThermostatVisible="dialogUpdateThermostatVisible" @closeDialogUpdateThermostatVisible="dialogUpdateThermostatVisible = false"/>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+import thermostatUpdate from './component/thermostatUpdate'
 export default {
-  name: 'thermostatSetting'
+  name: 'thermostatSetting',
+  components:{
+      thermostatUpdate
+  },
+  methods:{
+    updateThermostat(row){
+       this.thermostat = row
+       this.dialogUpdateThermostatVisible = true
+    }
+  },
+  data(){
+    return {
+      dialogUpdateThermostatVisible:false,
+      thermostat:{},
+          tableData: [{
+          name: '火焰量测',
+          pv: '5.68',
+          sv:'15.00',
+          upLimmit:'5.0',
+          lowLimmit:'5.0',
+          isAlarm:'否',
+          Light:'无',
+          status: '启用'
+        },{
+          name: '烘箱',
+          pv: '10.00',
+          sv:'14.00',
+          upLimmit:'10.0',
+          lowLimmit:'6.0',
+          isAlarm:'是',
+          Light:'无',
+          status: '启用'
+        },{
+          name: '加热箱',
+          pv: '9.12',
+          sv:'16.00',
+          upLimmit:'10.0',
+          lowLimmit:'7.0',
+          isAlarm:'否',
+          Light:'无',
+          status: '启用'
+        },{
+          name: '采样探头1',
+          pv: '5.00',
+          sv:'10.00',
+          upLimmit:'0.0',
+          lowLimmit:'0.0',
+          isAlarm:'无',
+          Light:'无',
+          status: '禁用'
+        },{
+          name: '伴热管1',
+          pv: '1.00',
+          sv:'10.00',
+          upLimmit:'10.0',
+          lowLimmit:'1.0',
+          isAlarm:'是',
+          Light:'无',
+          status: '启用'
+        },{
+          name: '采样探头2',
+          pv: '5.00',
+          sv:'10.00',
+          upLimmit:'0.0',
+          lowLimmit:'0.0',
+          isAlarm:'无',
+          Light:'无',
+          status: '禁用'
+        },{
+          name: '伴热管2',
+          pv: '1.00',
+          sv:'10.00',
+          upLimmit:'10.0',
+          lowLimmit:'1.0',
+          isAlarm:'是',
+          Light:'无',
+          status: '启用'
+        },{
+          name: '采样探头3',
+          pv: '5.00',
+          sv:'10.00',
+          upLimmit:'0.0',
+          lowLimmit:'0.0',
+          isAlarm:'无',
+          Light:'无',
+          status: '禁用'
+        },{
+          name: '伴热管3',
+          pv: '1.00',
+          sv:'10.00',
+          upLimmit:'10.0',
+          lowLimmit:'1.0',
+          isAlarm:'是',
+          Light:'无',
+          status: '启用'
+        },{
+          name: '采样探头4',
+          pv: '5.00',
+          sv:'10.00',
+          upLimmit:'0.0',
+          lowLimmit:'0.0',
+          isAlarm:'无',
+          Light:'无',
+          status: '禁用'
+        },{
+          name: '伴热管4',
+          pv: '1.00',
+          sv:'10.00',
+          upLimmit:'10.0',
+          lowLimmit:'1.0',
+          isAlarm:'是',
+          Light:'无',
+          status: '启用'
+        },,{
+          name: '采样探头5',
+          pv: '5.00',
+          sv:'10.00',
+          upLimmit:'0.0',
+          lowLimmit:'0.0',
+          isAlarm:'无',
+          Light:'无',
+          status: '禁用'
+        },{
+          name: '伴热管5',
+          pv: '1.00',
+          sv:'10.00',
+          upLimmit:'10.0',
+          lowLimmit:'1.0',
+          isAlarm:'是',
+          Light:'无',
+          status: '启用'
+        },,{
+          name: '采样探头6',
+          pv: '5.00',
+          sv:'10.00',
+          upLimmit:'0.0',
+          lowLimmit:'0.0',
+          isAlarm:'无',
+          Light:'无',
+          status: '禁用'
+        },{
+          name: '伴热管6',
+          pv: '1.00',
+          sv:'10.00',
+          upLimmit:'10.0',
+          lowLimmit:'1.0',
+          isAlarm:'是',
+          Light:'无',
+          status: '启用'
+        },]
+      }
+    }
 }
 </script>
 
